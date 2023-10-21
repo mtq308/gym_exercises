619608f97751a2bab0828d64b2e25cc5435ba12b
mtq308 <tranminhquoc945@gmail.com>
2023-10-21 22:23:57 +0700

    complete search exercise section & fetch data successful

---
+++ src/utils/fetchData.js
@@ -0,0 +1,13 @@
+export const fetchData = async (url, options) => {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data;
+}
+
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+        'X-RapidAPI-Key': 'efb8d9aab0msh6e38e02cae01829p1b34adjsn2e39a284582a',
+    }
+}
